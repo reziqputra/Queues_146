cc65324def5787d8372d0b4584c4cf1bf77b7604
TA1146 <muhammad.reziq.ft21@mail.umy.ac.id>
2022-12-12 17:42:38 +0800

    create function if to checks for the overflow condition

--- CircularQueues/Program.cs
+++ CircularQueues/Program.cs
@@ -1,6 +1,6 @@
 ﻿namespace CircularQueues
 {
-    class program
+    class Program
     {
         int FRONT, REAR, max = 5;
         int[] queue_array = new int[5];
@@ -10,6 +10,15 @@
              * the queue is initially empty.*/
             FRONT = -1;
             REAR = -1;
+        }
+        public void insert(int element)
+        {
+            /*This statement checks for the overflow condition. */
+            if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1))
+            {
+                Console.WriteLine("\nQueue overflow\n");
+                return;
+            }
         }
     }
 }
